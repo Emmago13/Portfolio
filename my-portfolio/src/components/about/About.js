3cf4d696959486e3a4d4a733687a9ebdfd8bcfa8
Emmanuel Gomez <80432917+Emmago13@users.noreply.github.com>
2026-01-09 12:37:15 -0300

    about fixes

--- my-portfolio/src/components/about/About.js
+++ my-portfolio/src/components/about/About.js
@@ -17,13 +17,13 @@ const About = () => {
                     <h2>Who I am</h2>
                     <img src={AboutImg} alt="about-img_mobile" title="image from unDraw" />
                     <p>
-                        Hey! I am Emmanuel Gomez, a 22 year old React frontend developer who is discovering the world of web development. I started this road more than a year ago and it’s been an awesome time working and learning about this amazing career.    
+                        Hey! I am Emmanuel Gomez, a 26 year old systems engineer student who is discovering the world of web development. I started this road more than a year ago and it’s been an awesome time working and learning about this amazing career.    
                     </p>
                     <p>
                         I'm trying to help people making life easier through the browser, working with designers and bringing ideas to life. I’m naturally curious and always learning something.
                     </p>
                     <p>
-                        I have experience working with HTML5, Css3, Sass, Bootstrap, JavaScript, React.js, Node.js, PHP and the framework Laravel.
+                        I have experience working with HTML5, Css3, Sass, Bootstrap, JavaScript, React.js, Node.js, Python, PHP and the framework Laravel.
                     </p>
                     <h4>If you are interested on collaborating or just talk </h4>
                     <WavyLink to="/contact" waveColor="#9C2D41" ><span>Start a conversation</span></WavyLink>
